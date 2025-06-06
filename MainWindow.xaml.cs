9978122ee311ac1b0deac1451c78320fbba8e87b
Sarty1234 <verm465@gmail.com>
2025-06-06 11:28:22 +0300

    thrust and comments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,52 +20,73 @@ namespace heli_stab
     /// </summary>
     public partial class MainWindow : Window
     {
-        [DllImport("user32.dll")]
-        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
-
+        // Here it includeskeybindevent for helicopter control
         [DllImport("user32.dll")]
         public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, IntPtr dwExtraInfo);
 
-        const UInt32 WM_KEYDOWN = 0x0100;
-        const UInt32 WM_KEYUP = 0x0202;
+
+        // Declaration of keys
         const int VK_RollLeft = 0x51; //Q
         const int VK_RollRight = 0x45; //E
         const int VK_PitchForward = 0x4F; //O
         const int VK_PitchBackward = 0x50; //P
+        const int VK_IncreaseThrust = 0x57; //W
+        const int VK_DecreaseThrust = 0x53; //S
 
-        System.Windows.Point rotation;
         public MainWindow()
         {
             InitializeComponent();
 
             Console.Beep(2000, 300);
 
-            DateTime time = DateTime.Now;
-            const int tpr = 250;
+            // --------------------Declaration of variables used in app to avoid huge memory leak--------------------
+            const int tpr = 250;        // Time beetwen each cycle
 
             RotationDetector rotationDetector = new RotationDetector();
             Process[] processes = Process.GetProcessesByName("SquadGame");
 
 
-            int targetPitch = 0;
-            int tartgetRoll = 0;
-            double threshold = 1;
-
+            DateTime time = DateTime.Now;
+            System.Windows.Point rotation;
 
             int pitchTime = 0;
             int rollTime = 0;
+            int thrustTime = 0;
+
+            double thrust = 0;
+
+            bool IsWorking = false;
+
+            // Declaration of targets to be acomplished
+            int targetPitch = 0;
+            int tartgetRoll = 0;
+            int targetThrust = 45;
+            double threshold = 1;
+
+            // App infinite cycle
             while (true)
             {
-                if (DateTime.Now > time.AddMilliseconds(tpr))
+                if (DateTime.Now > time.AddMilliseconds(tpr)) // Check if enouth time passed since last cycle
                 {
                     time = DateTime.Now;
 
+                    if (Keyboard.IsKeyDown(Key.NumPad1))
+                    {
+                        IsWorking = true;
+                    }
+                    else
+                    {
+                        IsWorking = false;
+                    }
+
+                    if (!IsWorking) continue;  // Check if key is pressed for app to work
+
+
+                    // Make screenshot and extract all data from it
                     MakeScreenshot();
-                    rotation = rotationDetector.GetRotation();
-                    output.Content = rotation.ToString();
+                    rotation = rotationDetector.GetRotation(out thrust);
 
-
-                    if (Keyboard.IsKeyDown(Key.N))
+                    if (Keyboard.IsKeyDown(Key.NumPad2))
                     {
                         targetPitch = 5;
                     } else
@@ -74,16 +95,24 @@ namespace heli_stab
                     }
 
 
+                    // Here it sets rotation speed for Roll based on how much is left to target rotation
                     if (tartgetRoll + rotation.X > 1 || tartgetRoll + rotation.X < -1) rollTime = 2;
                     else if (tartgetRoll + rotation.X > 5 || tartgetRoll + rotation.X < -5) rollTime = 2;
                     else if (tartgetRoll + rotation.X > 8 || tartgetRoll + rotation.X < -8) rollTime = 10;
 
+                    // Here it sets rotation speed for Pitch based on how much is left to target rotation
                     if (targetPitch + rotation.Y > 2 || targetPitch + rotation.Y < -2) pitchTime = 2;
                     else if (targetPitch + rotation.Y > 5 || targetPitch + rotation.Y < -5) pitchTime = 10;
+                    else if (targetPitch + rotation.Y > 8 || targetPitch + rotation.Y < -8) pitchTime = 20;
+
+                    // Here it sets change speed for Thrust based on how much is left to target thrust
+                    if (targetThrust - thrust > 2 || targetThrust - thrust < -2) thrustTime = 50;
+                    else if (targetThrust - thrust > 5 || targetThrust - thrust < -5) thrustTime = 100;
+                    else if (targetThrust - thrust > 8 || targetThrust - thrust < -8) thrustTime = 160;
 
 
 
-
+                    // Here it rotates Roll
                     if (tartgetRoll + rotation.X > threshold)
                     {
                         keybd_event(VK_RollLeft, 0, 0, IntPtr.Zero);
@@ -106,6 +135,7 @@ namespace heli_stab
                     }
 
 
+                    // Here it rotates Pitch
                     if (targetPitch + rotation.Y > threshold)
                     {
                         keybd_event(VK_PitchForward, 0, 0, IntPtr.Zero);
@@ -126,20 +156,47 @@ namespace heli_stab
 
                         Console.Beep(2000, 50);
                     }
+
+
+                    // Here it changes Thrust
+                    if (targetThrust - thrust > threshold)
+                    {
+                        keybd_event(VK_IncreaseThrust, 0, 0, IntPtr.Zero);
+
+                        Thread.Sleep(thrustTime);
+
+                        keybd_event(VK_IncreaseThrust, 0, 2, IntPtr.Zero);
+
+                        Console.Beep(2000, 50);
+                    }
+                    else if (targetThrust - thrust < -threshold)
+                    {
+                        keybd_event(VK_DecreaseThrust, 0, 0, IntPtr.Zero);
+
+                        Thread.Sleep(thrustTime);
+
+                        keybd_event(VK_DecreaseThrust, 0, 2, IntPtr.Zero);
+
+                        Console.Beep(2000, 50);
+                    }
                 }
             }
         }
 
 
 
-
+        // Declaration of data required for MakeScreenshot() to work to avoid memory leaks
         double screenLeft = SystemParameters.VirtualScreenLeft;
         double screenTop = SystemParameters.VirtualScreenTop;
         double screenWidth = SystemParameters.VirtualScreenWidth;
         double screenHeight = SystemParameters.VirtualScreenHeight;
         Bitmap bmp;
         Graphics g;
-        String filename;
+        String filename = "ScreenCapture.png";
+
+        /// <summary>
+        /// This function makes screenshot and saves it with name filename localy
+        /// </summary>
         private void MakeScreenshot()
         {
             using (bmp = new Bitmap((int)screenWidth,
@@ -147,7 +204,6 @@ namespace heli_stab
             {
                 using (g = Graphics.FromImage(bmp))
                 {
-                    filename = "ScreenCapture.png";
                     Opacity = .0;
                     g.CopyFromScreen((int)screenLeft, (int)screenTop, 0, 0, bmp.Size);
                     bmp.Save(filename, ImageFormat.Png);
--- RotationDetector.cs
+++ RotationDetector.cs
@@ -11,29 +11,44 @@ namespace heli_stab
 {
     public class RotationDetector
     {
+        // Number of pixels per full loadbar
         const int TenDegreePixelCount = 200;
+        const int HundredThrustPixelCount = 380;
 
+        // Start positions of heli loadbars
         System.Windows.Point RollStartPoint = new System.Windows.Point(960, 805);
         System.Windows.Point PitchStartPoint = new System.Windows.Point(1365, 540);
+        System.Windows.Point ThrustStartPoint = new System.Windows.Point(524, 712);  //(524; 332) - endpoint
 
+        // Data required for GetRotation() to work
         int LeftPixels = 0;
         int RightPixels = 0;
         int TopPixels = 0;
         int BottomPixels = 0;
+        int ThrustPixels = 0;
 
         Bitmap myBitmap;
         System.Windows.Point ReturnData;
 
-        public System.Windows.Point GetRotation()
+        public String filename = "ScreenCapture.png";
+
+        /// <summary>
+        /// This function gathers data from screenshot {filename} and returns it as (Roll; Pitch)
+        /// </summary>
+        /// <param name="thrustOutput">Outputs thrust value</param>
+        /// <returns></returns>
+        public System.Windows.Point GetRotation(out double thrustOutput)
         {
+            // Here it resets variables
             LeftPixels = 0;
             RightPixels = 0;
             TopPixels = 0;
             BottomPixels = 0;
+            ThrustPixels = 0;
 
 
-            //myBitmap = new Bitmap("ScreenCapture.png");
-            myBitmap = new Bitmap("ScreenCapture.png");
+            myBitmap = new Bitmap(filename);
+            //s = new Bitmap("E:\\c#_projects\\heli stab\\photos\\Screenshot (435).png");
 
             Color pixelColor = Color.FromArgb(255, 255, 254, 128);
 
@@ -90,9 +105,24 @@ namespace heli_stab
             }
 
 
+            //Thrust detection
+            for (int i = 0; true; i++)
+            {
+                if (myBitmap.GetPixel((int)ThrustStartPoint.X, (int)ThrustStartPoint.Y - i) == pixelColor)
+                {
+                    ThrustPixels++;
+                }
+                else
+                {
+                    break;
+                }
+            }
 
-            myBitmap.Dispose();
 
+            myBitmap.Dispose();  // It is very important to dispose of bitmap because if you don't there will be file access error
+
+            // Calculating and returning output data
+            thrustOutput = (double)ThrustPixels / (double)HundredThrustPixelCount * 100;
             ReturnData = new System.Windows.Point((double)(RightPixels - LeftPixels) / TenDegreePixelCount * 10, (double)(TopPixels - BottomPixels) / TenDegreePixelCount * 10);
             return ReturnData;
         }
